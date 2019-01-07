7c37ac5d82f9e7ddebe082a194f1efc02ed13386
dglund <jesper.dg@gmail.com>
2019-01-07 22:35:18 +0100

    minor fixes

--- piperdatabase/views.py
+++ piperdatabase/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, HttpResponse, HttpResponseRedirect
 from . import webscraper
 
-
 def PiperdatabaseView(request):
     return render(request, 'piperdatabase/database.html')
 
