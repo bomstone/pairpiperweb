626571816533128e23bdad82997183c8185c4b29
dglund <jesper.dg@gmail.com>
2019-01-09 19:25:50 +0100

    added 2 more charts with hard coded start dates

--- pairsdata/pipercharts.py
+++ pairsdata/pipercharts.py
@@ -1,11 +1,17 @@
+import matplotlib as mpl
+mpl.use('Agg')
+
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import statsmodels.api as sm
 
+import io
+import base64
+
 
 def fetch_data(asset, dataset, startdate, enddate):
-    df = pd.read_csv('fulldb.csv', index_col = 'date', parse_dates=True).append(pd.read_csv('livedb.csv', index_col = 'date', parse_dates=True))
+    df = pd.read_csv('fulldb.csv', index_col = 'date', parse_dates=True)#.append(pd.read_csv('livedb.csv', index_col = 'date', parse_dates=True))
     data = df.loc[startdate:enddate]
     pivot_table = data.pivot(columns='symbol',
                              values=dataset)
@@ -15,35 +21,48 @@ def fetch_data(asset, dataset, startdate, enddate):
 def zscore(series):
     return (series - series.mean()) / np.std(series)
 
-def plot_chart(symbol_list, start_date, end_date):
-    #Hämta data ur historisk databas+livedatabas
+def draw_chart(symbol_list, start_date, end_date):
+    # Hämta data ur historisk databas+livedatabas
     price = fetch_data(symbol_list, 'closing_price', start_date, end_date)
 
-    #Separera listan till två variabler
+    # Separera listan till två variabler
     S1 = price[symbol_list[0]]
     S2 = price[symbol_list[1]]
 
-    #Beräkna spread
+    # Beräkna spread
     S1 = sm.add_constant(S1)
     results = sm.OLS(S2, S1).fit()
-    S1 = S1[symbol_list[0]]   #återställer S1
+    S1 = S1[symbol_list[0]]  # återställer S1
     b = results.params[symbol_list[0]]
     spread = S2 - b * S1
 
-        #Beräkna omvänd spread
+    # Beräkna omvänd spread
     S2 = sm.add_constant(S2)
     results = sm.OLS(S1, S2).fit()
-    S2 = S2[symbol_list[1]]   #återställer S2
+    S2 = S2[symbol_list[1]]  # återställer S2
     b = results.params[symbol_list[1]]
     spread_rev = S1 - b * S2
 
-    #Beräkna och plotta Zscore
-    zscore(spread).plot(figsize=(16,3))
+    # Beräkna och plotta Zscore
+    zscore(spread).plot(figsize=(9, 2))
     zscore2 = zscore(spread_rev)
-    zscore2.plot(figsize=(16,3))
+    zscore2.plot(figsize=(9, 2))
     plt.axhline(zscore(spread).mean(), color='black')
     plt.axhline(1.0, color='red', linestyle='--')
     plt.axhline(-1.0, color='green', linestyle='--')
     plt.legend(['Spread ' + symbol_list[1], 'Spread ' + symbol_list[0]], bbox_to_anchor=(0., 1.02, 1., .102), loc=3,
                ncol=1, mode="expand", borderaxespad=0.);
-    #plt.savefig('assets/fig.png')
+
+    f = io.BytesIO()
+
+    plt.savefig(f,
+                format="png",
+                facecolor=('white'),
+                bbox_inches='tight',
+                )
+    plt.clf()
+
+    image_b64 = base64.b64encode(f.getvalue())  # konverterar bilden till base64
+    image_show = str(image_b64, 'utf8')  # encodar till 8 bit strängar
+
+    return image_show
