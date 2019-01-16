baa381e00f5850c682d6346d07876acae54df5e3
dglund <jesper.dg@gmail.com>
2019-01-16 18:21:01 +0100

    fixed views imports

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, HttpResponse, HttpResponseRedirect
-
+from django.views.generic import TemplateView
 
 class PortfolioView(TemplateView):
     template_name = 'portfolio/portfolio.html'
