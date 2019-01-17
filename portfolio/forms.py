74962414f8264c2a519ab36c414a5805b3ae8607
dglund <jesper.dg@gmail.com>
2019-01-17 23:19:43 +0100

    set 'required' to false

--- portfolio/forms.py
+++ portfolio/forms.py
@@ -1,18 +1,18 @@
 from django import forms
 
 class AddPosition(forms.Form):
-    strategy = forms.ChoiceField()
-    legs = forms.IntegerField()
+    strategy = forms.ChoiceField(required=False)
+    legs = forms.IntegerField(required=False)
 
-    start_date = forms.DateField()
-    start_time = forms.TimeField()
-    exposure = forms.DecimalField()
+    start_date = forms.DateField(required=False)
+    start_time = forms.TimeField(required=False)
+    exposure = forms.DecimalField(required=False)
 
-    ticker = forms.CharField()
-    ul_open = forms.DecimalField()
-    mf_open = forms.DecimalField()
-    mf_finlevel = forms.DecimalField()
-    quantity = forms.IntegerField()
-    commission = forms.BooleanField()
+    ticker = forms.CharField(required=False)
+    ul_open = forms.DecimalField(required=False)
+    mf_open = forms.DecimalField(required=False)
+    mf_finlevel = forms.DecimalField(required=False)
+    quantity = forms.IntegerField(required=False)
+    commission = forms.BooleanField(required=False)
 
 
