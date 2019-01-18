b58c15e98180338a01367c43828a9f4a309ad2a3
dglund <jesper.dg@gmail.com>
2019-01-18 13:39:20 +0100

    fixed settings.py again

--- pairpiperweb/settings.py
+++ pairpiperweb/settings.py
@@ -81,11 +81,11 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    },
 
     'piperdb': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.path.join(BASE_DIR, 'piperdb'),
+        'NAME': os.path.join(BASE_DIR, 'piperdb.sql'),
         'USER': 'root',
         'PASSWORD': ''
     }
