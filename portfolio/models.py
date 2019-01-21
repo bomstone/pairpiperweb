5260ccce4f8e69abf28821b0d4edf3b3fab306fc
dglund <jesper.dg@gmail.com>
2019-01-21 20:15:44 +0100

    continued views and models

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class PortfolioPart(models.Model):
+    trade_id = models.IntegerField()
+    strategy = models.CharField(max_length=30)
+    product_type = models.CharField(max_length=30)
+    open_date = models.CharField(max_length=30)
+    open_time = models.CharField(max_length=30)
+    true_exposure = models.DecimalField(max_digits=10, decimal_places=3)
+
+    asset = models.CharField(max_length=30)
+    ul_open = models.DecimalField(max_digits=10, decimal_places=3)
+    open_price = models.DecimalField(max_digits=10, decimal_places=3)
+    mf_finlevel = models.DecimalField(max_digits=10, decimal_places=3)
+    quantity = models.IntegerField()
+    commission = models.DecimalField(max_digits=10, decimal_places=3)
+
+    class Meta():
+        db_table = 'portfolio'
+
+    def __str__(self):
+        return self.strategy
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render, HttpResponse, HttpResponseRedirect
 from django.views.generic import TemplateView
+from .models import PortfolioPart
 
 
 class PortfolioView(TemplateView):
     template_name = 'portfolio/portfolio.html'
+    def PrintPortfolio(request):
+        template_name = 'portfolio/portfolio.html'
 
+        queryset = PortfolioPart.objects.all()
+        context = {
+            'object_list': queryset,
+            'title:': 'List'
+        }
+
+        return render(request, self.template_name, context)
+'''
     variable1 = 'test1'
     variable2 = 'test2'
 
@@ -14,5 +25,5 @@ class PortfolioView(TemplateView):
         context['variable2'] = self.variable2
         return context
 
-        return render(request, self.template_name, context)
+        return render(request, self.template_name, context)'''
 
