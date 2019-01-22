b87089146e891b4a32a3ede88acf41d10158fde0
dglund <jesper.dg@gmail.com>
2019-01-23 00:02:44 +0100

    added models to piperdatabase

--- piperdatabase/models.py
+++ piperdatabase/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class PiperDatabase(models.Model):
+    date = models.CharField(max_length=30, null=False)
+    asset = models.CharField(max_length=30, null=False)
+    open_price = models.DecimalField(max_digits=10, decimal_places=3, null=False)
+    high_price = models.DecimalField(max_digits=10, decimal_places=3, null=False)
+    low_price = models.DecimalField(max_digits=10, decimal_places=3, null=False)
+    close_price = models.DecimalField(max_digits=10, decimal_places=3, null=False)
+
+    class Meta():
+        db_table = 'piperdb'
