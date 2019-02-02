e69045a360a2dbbe4ccf8bede83630282af186ca
dglund <jesper.dg@gmail.com>
2019-02-02 21:30:35 +0100

    changed subposition-form to modelform and extendend to modelformset

--- addposition/forms.py
+++ addposition/forms.py
@@ -29,9 +29,52 @@ class MainpositionModelForm(forms.ModelForm):
         instance.save()
         return instance
 
+
+class SubpositionModelForm(forms.ModelForm):
+    class Meta:
+        model = PortfolioModel
+        fields = [
+            'product_type',
+            'open_date',
+            'open_time',
+            'asset',
+            'ul_open',
+            'open_price',
+            'mf_finlevel',
+            'quantity',
+        ]
+
+    # Override save-funktion och lägger till ytterligare metadata till positionen som ej fylls i genom formulär
+    def save(self):
+        instance = super(SubpositionModelForm, self).save(commit=False)
+
+        check_tradeid = PortfolioModel.objects.all().aggregate(Max('trade_id'))
+        trade_id = check_tradeid.get('trade_id__max')
+
+        insert_type = 'subposition'
+        #open_price = self.cleaned_data['open_price']
+        #ul_open = self.cleaned_data['ul_open']
+        #quantity = self.cleaned_data['quantity']
+        #currency = 'SEK'
+        #strategy = strategy_in
+        #user = user_in
+
+        instance.trade_id = trade_id
+        instance.insert_type = insert_type
+        #instance.net_open_sek = open_price * quantity
+        #instance.true_exposure = (open_price * quantity) * (ul_open / open_price)
+        #instance.currency = currency
+        #instance.strategy = strategy
+        #instance.user = user
+
+        instance.save()
+        return instance
+
+
+
 SubpositionFormset = modelformset_factory(
     PortfolioModel,
-    fields=('product_type', 'open_date', 'open_time', 'asset', 'ul_open', 'open_price', 'mf_finlevel', 'quantity'),
+    form=SubpositionModelForm,
     extra=2,
 )
 
