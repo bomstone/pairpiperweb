a5c4967a038cdb9aa2815aa373109ba170414f13
dglund <jesper.dg@gmail.com>
2019-02-03 12:30:32 +0100

    portfolio now show fields for objects

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,23 +1,24 @@
 from django.shortcuts import render, HttpResponse, HttpResponseRedirect
 from django.views.generic import TemplateView
 from .models import PortfolioModel
+from .forms import PortfoliopositionFormset
 
 
 def PortfolioView(request):
     template_name = 'portfolio/portfolio.html'
 
     if request.method == "GET":
-        queryset = PortfolioModel.objects.filter(trade_id=2)
+        portfolioposition = PortfoliopositionFormset(queryset=PortfolioModel.objects.all())
 
         context = {
-            'object_list': queryset,
+            'portfolioposition': portfolioposition,
         }
 
         return render(request, template_name, context)
 
     elif request.method == "POST":
 
-        queryset = PortfolioModel.objects.filter(trade_id=2)
+        queryset = PortfolioModel.objects.all()
 
         context = {
             'object_list': queryset,
